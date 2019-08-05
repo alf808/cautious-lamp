476c0125b7a9b92a115a024ec28388e8d3cf2fad
alf808 <alf@akademe.net>
2019-08-05 02:39:57 -1000

    attempted threading

--- client.py
+++ client.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 import socket
+import sys
+
+port = int(sys.argv[1])
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server_addr = ('127.0.0.1', 8888)
+server_addr = ('127.0.0.1', port)
 print('connecting to {} port {}'.format(server_addr[0], server_addr[1]))
 sock.connect(server_addr)
 
-odd_data = "now is the time to fine all men."
+odd_data = 'GET /index.html HTTP/1.1\r\nHost: google.com\r\n\r\n'
 # encode into byte string to send to server
 sock.send(odd_data.encode())
 
